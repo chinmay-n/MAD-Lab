e4754f0ac7677b1172b6c62456f3fcf9644070fc
Chinmay <mit.chinmayn@gmail.com>
2017-01-12 16:27:00 +0530

    Second commit

--- lab1/app/src/main/java/com/example/student/myapplication/MainActivity.java
+++ lab1/app/src/main/java/com/example/student/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.student.myapplication;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b= (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.content_main2);
+            }
+        });
     }
 }
